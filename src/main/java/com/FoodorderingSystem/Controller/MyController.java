4d33fb4f7f84b15e2040270e099b7c31461fdd69
63alok63 <63alok63@gmail.com>
2022-06-28 10:55:35 +0530

    final version

--- src/main/java/com/FoodorderingSystem/Controller/MyController.java
+++ src/main/java/com/FoodorderingSystem/Controller/MyController.java
@@ -3,6 +3,7 @@ package com.FoodorderingSystem.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,8 @@ import com.FoodorderingSystem.Service.MyService;
 
 
 @RestController
+@CrossOrigin(origins = "*")
+ 
 public class MyController {
 
 	@Autowired
